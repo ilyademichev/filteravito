a168fd08ace1d8aad7c2a4120dcf95995a7eeab8
ilya <ilya.demichev.freelance@gmail.com>
2020-05-29 16:05:45 +0000

    scrape the daily list of ads =occasional timeout failuries =reached error page errors

--- filterday.py
+++ filterday.py
@@ -1,3 +1,4 @@
+import tempfile
 import re
 import time
 from typing import Optional, Match
@@ -14,13 +15,16 @@ useragent = 'Mozilla/5.0 (Linux; U; Android 4.4.2; en-us; SCH-I535 Build/KOT49H)
 
 # dcap = dict(DesiredCapabilities.PHANTOMJS)
 # dcap["firefox.page.settings.userAgent"] = (useragent)
+scrshotpath = '/home/ilya/scrshotavito'
 https_proxy_domain = "93.159.236.30"
 https_proxy_port = "8080"
 https_proxy = https_proxy_domain + ":" + https_proxy_port
-profile = webdriver.FirefoxProfile("/home/ilya/.mozilla/firefox/vfwzppqq.avitoproxy")
-
-
-#proxy server settings
+profile = webdriver.FirefoxProfile("/home/ilya/.mozilla/firefox/vfwzppqq.avitoproxy")#asdf
+#locations
+#https://www.avito.ru/maloyaroslavets/nedvizhimost?s=104
+#https://www.avito.ru/obninsk/nedvizhimost?s=104
+sortedItemsLocationLink = 'https://m.avito.ru/maloyaroslavets/nedvizhimost?s=104'
+# proxy server settings
 
 # webdriver.DesiredCapabilities.FIREFOX['proxy']={
 #     "httpProxy":https_proxy,
@@ -39,7 +43,7 @@ profile = webdriver.FirefoxProfile("/home/ilya/.mozilla/firefox/vfwzppqq.avitopr
 #     'noProxy': '' # set this value as desired
 #     })
 
-#fake user agent
+# fake user agent
 # profile.set_preference("general.useragent.override", useragent)
 # profile.set_preference("network.proxy.type", 1)
 # profile.set_preference("network.proxy.http", https_proxy)
@@ -50,76 +54,98 @@ profile = webdriver.FirefoxProfile("/home/ilya/.mozilla/firefox/vfwzppqq.avitopr
 # dcap = dict(DesiredCapabilities.FIREFOX)
 # dcap["firefox.page.settings.userAgent"] = (useragent)
 
-profile.set_preference("general.useragent.override",useragent )
+profile.set_preference("general.useragent.override", useragent)
 options = Options()
-options.headless = False
-driver = Firefox(options = options,firefox_profile = profile  )
+options.headless = True
+driver = Firefox(options=options, firefox_profile=profile)
+driver.set_page_load_timeout(240)
 
-driver.get('https://m.avito.ru/obninsk/nedvizhimost?sort=date')
-#reference to the date in html code
-timestampxpath = "//div[@data-marker='item/datetime']"
-#reference to the realty item that stands for flat
-appartmentxpath = "//a[contains(text(),'квартира')]"
-#neccessary delay to load up the page
-SCROLL_PAUSE_TIME = 0.5
 
-# Get scroll height
-last_height = driver.execute_script("return document.body.scrollHeight")
+# rotate proxies technique should be applied to eliminate banning
+#
 
-while True:
-    # Scroll down to bottom
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+# splits the string list of timestamps into tuple list ( day ,time )
+#timestamps_list : list of timestamps in string format
+def split_timestamps(timestamps_list):
+    t = tuple()
+    tp = list()
+    for ts in timestamps_list:
+        m: Optional[Match[str]] = re.match(r'(?P<day>^.*), (?P<timestamp>\d{2}:\d{2})', ts)
+        if m:
+            t = (m.group('day'), m.group('timestamp'))
+            print(t)
+            tp.append(t)
+    return tp
 
-    # Wait to load page
-    time.sleep(SCROLL_PAUSE_TIME)
 
-    # Calculate new scroll height and compare with last scroll height
-    new_height = driver.execute_script("return document.body.scrollHeight")
-    if new_height == last_height:
-        break
-    last_height = new_height
-#parse links that are preenst after scroll
-time.sleep(20)
-realtylinks = driver.find_elements_by_xpath(appartmentxpath)
-time.sleep(20)
-#print em out
-print(*( map(lambda x: x.get_attribute('href'), realtylinks ) ), sep='\n')
-time.sleep(20)
-#parse datastamps
-timestamp = driver.find_elements_by_xpath(timestampxpath)
-time.sleep(20)
-ls = list(map(lambda x: x.text, timestamp ))
-#print them
-print( ls)
-#split date into day an time
-for ts in ls:
-    m: Optional[Match[str]] = re.search(r'(?P<day>^.*), (?P<timestamp>\d{2}:\d{2})', ts)
-    if m :
-        print( m.group('day'))
-        print( m.group('timestamp'))
-allday = False
-#initialization
-#timestamp of the last item is set
-#scroll till the yesterday mark is not presend
-#to discover all day listing of ads
-while not allday :
+try:
+    driver.get(sortedItemsLocationLink)
+    # reference to the date in html code
+    timestampxpath = "//div[@data-marker='item/datetime']"
+    # reference to the realty item that stands for flat
+    appartmentxpath = "//a[contains(text(),'квартира')]"
+    # neccessary delay to load up the page
+    SCROLL_PAUSE_TIME = 0.5
 
-    m= re.search(r'(?P<day>^.*), (?P<timestamp>\d{2}:\d{2})', ls[-1])
-    if m :
-        #quit once the mark found
-        if m.group('day') == 'Вчера' :
+    # Get scroll height
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+        # Scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        # Wait to load page
+        time.sleep(SCROLL_PAUSE_TIME)
+
+        # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+    # parse links that are present after scroll
+    time.sleep(20)
+    realtylinks = driver.find_elements_by_xpath(appartmentxpath)
+    time.sleep(20)
+    # print em out
+    print(*(map(lambda x: x.get_attribute('href'), realtylinks)), sep='\n')
+    time.sleep(20)
+    # parse datastamps
+    timestamp = driver.find_elements_by_xpath(timestampxpath)
+    time.sleep(20)
+    ls = list(map(lambda x: x.text, timestamp))
+    # print them
+    print(ls)
+    # split date into day an time
+    allday = False
+    # initialization
+    # timestamp of the last item is set
+    # scroll till the yesterday mark is not presend
+    # to discover all day listing of ads
+    while not allday:
+        t = split_timestamps(ls)
+        print(t)
+        #get day tags and make up  a set
+        s = set(  map(lambda x: x[0], t ) )
+        #    m= re.match(r'(?P<day>^.*), (?P<timestamp>\d{2}:\d{2})', ls[-1])
+        if 'Вчера' in s:
+            # quit once the mark found
             allday = True
         else:
-            #click a button to expand the list
-            loadmorebuttonxpath = "//span[contains(text(),'Загрузить еще')]"
-            loadmorebutton = driver.find_element_by_xpath(loadmorebuttonxpath)
-            time.sleep(3)
-            loadmorebutton.click()
-            time.sleep(3)
+                # click a button to expand the list
+                loadmorebuttonxpath = "//span[contains(text(),'Загрузить еще')]"
+                loadmorebutton = driver.find_element_by_xpath(loadmorebuttonxpath)
+                time.sleep(3)
+                loadmorebutton.click()
+                time.sleep(3)
+                timestamp = driver.find_elements_by_xpath(timestampxpath)
+                time.sleep(10)
+                ls = list(map(lambda x: x.text, timestamp))
 
-            timestamp = driver.find_elements_by_xpath(timestampxpath)
-            time.sleep(10)
-            ls = list(map(lambda x: x.text, timestamp))
+# clean up the selenium driver resources in any case
+# memory leak could occur once not completed gracefully
 
-
-driver.quit()
+finally:
+    tmp = scrshotpath  + tempfile.NamedTemporaryFile().name + ".png"
+    print(tmp)
+    driver.get_screenshot_as_file(tmp)
+    driver.quit()
