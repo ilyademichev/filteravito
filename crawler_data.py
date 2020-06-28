3557f8f05b6f39df55eb2739b4690ced6cf2bae8
ilya <ilya.demichev.freelance@gmail.com>
2020-06-28 05:28:48 +0000

    is_enabled(self, by_locator):         return WebDriverWait(selfTO FIX:.returns not boolean type for load more button

--- crawler_data.py
+++ crawler_data.py
@@ -7,7 +7,7 @@ class CrawlerData:
     YESTERDAY_TAG = 'Вчера'
     IMPLICIT_TIMEOUT_INT_SECONDS = 300
     SCRSHOT_PATH = '/home/ilya/scrshotavito'
-    ATTEMPTS_INT = 10
+    ATTEMPTS_INT = 3
     SCROLL_PAUSE_TIME = 0.5
 
 
